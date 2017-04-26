95ff8c7ce239de8477a130edb30e4c66289d5c93
SuperLazyDog <jyoitatsu@foxmail.com>
2017-04-27 02:40:07 +0900

    skip some, just overview

--- 第十四章.playground/Contents.swift
+++ 第十四章.playground/Contents.swift
@@ -3,4 +3,48 @@
 import UIKit
 
 
-Int
+func mySwap<T>(_ a: inout T, _ b: inout T) {
+    let t = a;
+    a = b;
+    b = t;
+}
+
+var testDic: [Int : String] = [1: "one"]
+testDic[1]
+testDic[4] = "four"
+print(testDic)
+/*func +<Key, Value>(lhs: [Key : Value], rhs: [Key : Value]) -> [Key : Value] {
+    var dic = lhs
+    for (k,v) in rhs {
+        dic
+    }
+}*/
+
+struct PickForever<T> {
+    var list: [T]
+    var index = 0
+    init(_ a:[T]) {
+        list = a
+    }
+    mutating func pick() -> T {
+        if index >= list.count {
+            index = 0
+        }
+        let rtn = list[index]
+        index += 1
+        return rtn
+    }
+}
+
+extension PickForever where T: Comparable {
+    mutating func sort() {
+        list.sort()
+    }
+}
+
+extension PickForever where T: Hashable {
+    mutating func makeUnique() {
+        let tmp = Set<T>(list)
+        list = [T](tmp)
+    }
+}
