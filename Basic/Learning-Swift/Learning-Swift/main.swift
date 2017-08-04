617ffb74d7d2090b98e94363df162bf7bad606f8
Xu Weida <jyoitatsu@foxmail.com>
2017-08-04 17:31:50 +0900

    start review Swift

---
+++ Basic/Learning-Swift/Learning-Swift/main.swift
@@ -0,0 +1,12 @@
+//
+//  main.swift
+//  Learning-Swift
+//
+//  Created by Weida Xu on 2017/8/4.
+//  Copyright © 2017年 Weida Xu. All rights reserved.
+//
+
+import Foundation
+
+print("Hello, World!")
+
