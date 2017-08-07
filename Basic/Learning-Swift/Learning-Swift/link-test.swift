f30ecbeccfdd6f937ba8b56f89a12b2d01023f08
SuperLazyDog <jyoitatsu@foxmail.com>
2017-08-07 14:12:20 +0900

    开始复习并进行了链接测试

---
+++ Basic/Learning-Swift/Learning-Swift/link-test.swift
@@ -0,0 +1,20 @@
+//
+//  link-test.swift
+//  Learning-Swift
+//
+//  Created by Weida Xu on 2017/8/7.
+//  Copyright © 2017年 Weida Xu. All rights reserved.
+//
+
+//------------------------------------------------------------------------------
+//                       总结：swift内文件的链接不需要任何操作
+//------------------------------------------------------------------------------
+import Foundation
+
+class LinkTest {
+    var a: Int = 1
+    
+    func putsA() {
+        print(a);
+    }
+}
