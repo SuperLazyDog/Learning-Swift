878e8ab33a2eadee833606a10c9ca9e1e88d7ee4
SuperLazyDog <jyoitatsu@foxmail.com>
2018-02-17 15:23:24 +0800

    Done-step: [完成项目结构]

---
+++ Learning-Swift 4/Learning-Swift 4/S1 Swiftでプログラミング/S1_Basic.swift
@@ -0,0 +1,16 @@
+//
+//  S1_Basic.swift
+//  Learning-Swift 4
+//
+//  Created by 徐伟达 on 2018/2/17.
+//  Copyright © 2018年 徐伟达. All rights reserved.
+//
+
+import Foundation
+
+func S1BasicTest() {
+	print("//-------------------------------------------")
+	print("//           S1 Swiftでプログラミング")
+	print("//-------------------------------------------")
+	print("hello in s1")
+}
