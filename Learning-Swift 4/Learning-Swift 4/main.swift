171c16e5f45e8f2fec304abd3665bac172159a59
SuperLazyDog <jyoitatsu@foxmail.com>
2018-02-17 15:06:03 +0800

    Temp: [第一章进行中]

--- Learning-Swift 4/Learning-Swift 4/main.swift
+++ Learning-Swift 4/Learning-Swift 4/main.swift
@@ -9,4 +9,6 @@
 import Foundation
 
 print("Hello, World!")
+var a : Int = 1234, b : Int = 1234; var c : Double = 1.2;
+print(<#T##items: Any...##Any#>, separator: <#T##String#>, terminator: <#T##String#>)
 
