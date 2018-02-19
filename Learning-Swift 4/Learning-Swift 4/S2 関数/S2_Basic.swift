24b783f19f1e3fa0067eecdc37f300542455601f
SuperLazyDog <jyoitatsu@foxmail.com>
2018-02-19 14:29:15 +0800

    Done-step: [完成函数定义的选项 p48]

--- Learning-Swift 4/Learning-Swift 4/S2 関数/S2_Basic.swift
+++ Learning-Swift 4/Learning-Swift 4/S2 関数/S2_Basic.swift
@@ -15,6 +15,7 @@ func S2BasicTest() {
 	//---------------------------------------------
 	//               関数定義の基本
 	//---------------------------------------------
+	// 基本定义
 //	func test(a: Int, _:Bool) {}
 //	func test(aa: Int, _ a: Bool) {}
 //	test(a: <#T##Int#>, <#T##Bool#>)
@@ -22,10 +23,25 @@ func S2BasicTest() {
 	//---------------------------------------------
 	//          関数定義におけるさまざまな設定
 	//---------------------------------------------
-	
+	// 改变参数自身 inout
+//	func test2(sample a: inout Int) {} // 定义
+//	test2(sample: &<#T##Int#>) // 呼出
+	// 默认值
+//	func test2_2(_ a: Int = 1, _ b: Int = 2, _ c: Int = 3, d: Int _ e: Int = 5) {}
 	//---------------------------------------------
 	//               オーバーロード
 	//---------------------------------------------
+	func test2_3(a: Int) {}
+	
+	//重载版本
+//	func test2_3(a: Bool) {} // 参数类型, 不能只有参数类型不同
+	func test2_3(a: Int, b: Int) {} // 参数数量
+	func test2_3(labelSample a: Int) {} // 标签
+//	func test2_3(aa a: Int) -> Bool { return false } // 返回值类型, 不能只是返回值不同
+	
+	// 参数类型不同的测试 结论:不能只有参数类型不同
+//	func compare(_ a: Int, _ b: Int) -> Bool {return false}
+//	func compare(_ a: String, _ b: String) -> Bool {return false}
 	
 	//---------------------------------------------
 	//                 タプル
@@ -39,3 +55,11 @@ func S2BasicTest() {
 	//               演算子の定義
 	//---------------------------------------------
 }
+
+//类型不同的重载不能用在被嵌套函数
+//func test2_3(a: Bool) {}
+//func test2_3(a: Int) {}
+//
+//func mySwap(_ a: inout String, _ b: inout String) {}
+//func mySwap(_ a: inout Int, _ b: inout Int) {}
+
