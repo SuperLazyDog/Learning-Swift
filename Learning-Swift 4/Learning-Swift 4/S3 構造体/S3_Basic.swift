b76140b7b58aa2c1e9d5dcdeaa7f4cd523398230
SuperLazyDog <jyoitatsu@foxmail.com>
2018-02-21 21:59:32 +0800

    Done-step: [结束3.1 构造体定义 p75]

--- Learning-Swift 4/Learning-Swift 4/S3 構造体/S3_Basic.swift
+++ Learning-Swift 4/Learning-Swift 4/S3 構造体/S3_Basic.swift
@@ -15,6 +15,28 @@ func S3BasicTest() {
 	print("//-------------------------------------------")
 	print("//                S3 構造体")
 	print("//-------------------------------------------")
+	//---------------------------------------------
+	//               構造体の定義
+	//---------------------------------------------
+	struct MyTest3_1 {
+		var intA: Int
+		var intB: Int
+		var isNeedUpdata: Bool
+		
+		init(asDefault _: Bool) {
+			self.intA = 1
+			self.intB = 1
+			isNeedUpdata = false;
+		}
+		
+		init(intA a: Int, intB b: Int) {
+			self.init(asDefault: true)
+			isNeedUpdata = !isNeedUpdata ? !isNeedUpdata : isNeedUpdata
+			self.intA = a
+			self.intB = b
+		}
+	}
+	
 }
 
 //------------------------------------------------------------------
