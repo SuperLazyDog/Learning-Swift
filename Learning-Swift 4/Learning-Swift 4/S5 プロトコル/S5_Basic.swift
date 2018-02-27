f7d7b458e1bace50fda36cfb6558772c7c713ae1
SuperLazyDog <jyoitatsu@foxmail.com>
2018-02-27 22:16:39 +0800

    Done-step: [完成5.3 协议与附属型]

--- Learning-Swift 4/Learning-Swift 4/S5 プロトコル/S5_Basic.swift
+++ Learning-Swift 4/Learning-Swift 4/S5 プロトコル/S5_Basic.swift
@@ -29,6 +29,26 @@ func S5BasicTest() {
 	//               プロトコルの宣言
 	//---------------------------------------------
 	
+	//---------------------------------------------
+	//              プロトコルと付属型
+	//---------------------------------------------
+	struct MyTest5_3: SampleProtocol5_2, CustomStringConvertible, CustomDebugStringConvertible {
+		var data: Int = 0
+		func merge(newOne: MyTest5_3) -> MyTest5_3 {
+			return MyTest5_3(data: self.data + newOne.data)
+		}
+		var description: String {
+			return String.init(describing: self.data)
+		}
+		
+		var debugDescription: String {
+			return "Debug" + String.init(describing: self.data)
+		}
+	}
+	let myTest5_3: MyTest5_3 = MyTest5_3()
+	let myTest5_3_2 = myTest5_3.merge(newOne: MyTest5_3(data: 9))
+	print(myTest5_3, myTest5_3_2, separator: "\n")
+	
 }
 
 //------------------------------------------------------------------
@@ -39,7 +59,21 @@ protocol SampleProtocol {
 //	static var sampleString2: String { get }
 //	static func + (lhs: Self, rhs: Self) -> Int
 }
+//------------------------------------------------------------------
+//                    プロトコルと付属型　補助エリア
+//------------------------------------------------------------------
+protocol SampleProtocol5_2 {
+	func merge(newOne: Self) -> Self
+}
 
+//系统自带protocol: Equatable
+//public protocol Equatable {
+//	static func ==(lhs:Self, rhs: Self) -> Bool
+//}
+
+protocol SampleProtocol5_2_2 {
+	associatedtype SampleData: Equatable where SampleData: CustomStringConvertible, SampleData: CustomDebugStringConvertible
+}
 //------------------------------------------------------------------
 //                    ....　補助エリア
 //------------------------------------------------------------------
