686d5111af4583cdf7c8315bacc6026dc09a6e72
SuperLazyDog <jyoitatsu@foxmail.com>
2018-03-07 06:17:10 +0800

    Done-step: [完成第八章 类与继承 p224]

--- Learning-Swift 4/Learning-Swift 4/S8 クラスと継承/S8_Basic.swift
+++ Learning-Swift 4/Learning-Swift 4/S8 クラスと継承/S8_Basic.swift
@@ -42,10 +42,42 @@ func S8BasicTest() {
 	NullClass.sayHello()
 	FirstChildClass.sayHello()
 	SecondChildClass.sayHello() // 这个的clas属性/方法没改写
+	
+	//---------------------------------------------
+	//               イニシャライザ
+	//---------------------------------------------
+	
+	//---------------------------------------------
+	//             継承とサブクラスの定義
+	//---------------------------------------------
+	print("\n\n")
+	print(SubscriptSample2.init()[2] ?? "nil")
+	
+	//---------------------------------------------
+	//                 クラスと型
+	//---------------------------------------------
+	print(FirstChildClass.self == type(of: firstChildClass))
+	print(type(of: Int(1)))
+	print(type(of: Swift4BasicLearningSelector.S1))
+//	print(FirstChildClass.self.Type == FirstChildClass.Type)
+	let sampleProtocolType: CustomStringConvertible.Type = ClassAndTypeTest.self
+	print(sampleProtocolType)
+	// 类与协议的合成, 表示采用协议的类或其子类
+	typealias Sample = ClassAndTypeTest & CustomStringConvertible & Equatable
+	//---------------------------------------------
+	//                 解放時処理
+	//---------------------------------------------
+	var deinitTest: DeinitSampleClass? = DeinitSampleClass()
+	deinitTest = nil
+	deinitTest = DeinitSampleClass()
+	deinitTest = nil
+	deinitTest = DeinitSampleClass()
+	var _ = deinitTest // 消除被改写但没有被读取的警告
+	
 }
 
 //------------------------------------------------------------------
-//                    クラス定義　補助エリア
+//                     クラス定義　補助エリア
 //------------------------------------------------------------------
 protocol ClassNameAchievable {
 	var className: String { get }
@@ -94,6 +126,59 @@ class SecondChildClass: FirstChildClass {
 	}
 }
 
+//------------------------------------------------------------------
+//                   イニシャライザ　補助エリア
+//------------------------------------------------------------------
+class BasicInitializerClass {
+	var a: String
+	init(a: String) {
+		self.a = a
+	}
+}
+
+class FirstChildInitializerClass: BasicInitializerClass {
+	var b: String
+	init(a: String, b: String) {
+		self.b = b
+		super.init(a: a)
+	}
+	override convenience init(a: String) {
+		self.init(a: a, b: "")
+	}
+}
+
+//------------------------------------------------------------------
+//                  継承とサブクラスの定義　補助エリア
+//------------------------------------------------------------------
+class SubscriptSample {
+	var array: Array<Int> = [1, 2, 3, 4, 5*5]
+	subscript(id: Int) -> Int? {
+		return id < array.count ? array[id] : nil
+	}
+}
+
+class SubscriptSample2: SubscriptSample {
+	override subscript(id: Int) -> Int? {
+		return id < self.array.count ? (super[id]! * super[id]!) : nil
+	}
+}
+
+
+//------------------------------------------------------------------
+//                    クラスと型　補助エリア
+//------------------------------------------------------------------
+class ClassAndTypeTest: CustomStringConvertible {
+	var description: String = "\(ClassAndTypeTest.self)"
+}
+
+//------------------------------------------------------------------
+//                    解放時処理　補助エリア
+//------------------------------------------------------------------
+class DeinitSampleClass {
+	deinit {
+		print("deinited")
+	}
+}
 //------------------------------------------------------------------
 //                    ....　補助エリア
 //------------------------------------------------------------------
