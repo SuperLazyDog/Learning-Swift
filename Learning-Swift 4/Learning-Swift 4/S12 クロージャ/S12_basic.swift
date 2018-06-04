a485df8129c273f587e0c52564f06d806c94e106
SuperLazyDog <jyoitatsu@foxmail.com>
2018-06-05 03:26:33 +0900

    Done-step: [12.1 闭包的声明]

--- Learning-Swift 4/Learning-Swift 4/S12 クロージャ/S12_basic.swift
+++ Learning-Swift 4/Learning-Swift 4/S12 クロージャ/S12_basic.swift
@@ -15,6 +15,20 @@ func S12BasicTest() {
 	print("//-------------------------------------------")
 	print("//                S12 クロージャ")
 	print("//-------------------------------------------")
+	let testClosure1 = { () in
+		print("here is testClosure")
+	}
+	testClosure1()
+	var testClosure2: ((Int)->Int)!
+	testClosure2 = { (a: Int) -> Int in
+		if a <= 0 {
+			return a
+		} else {
+			return a + testClosure2(a-1)
+		}
+	}
+	print(testClosure2(100))
+
 }
 
 //------------------------------------------------------------------
